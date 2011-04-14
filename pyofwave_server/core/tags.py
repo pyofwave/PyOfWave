fcf0a875a48596027bf7181d3b14fc30d1c20596
alcinnz <alcinnz@eml.cc>
2011-04-13 20:36:26 -0700

    Implementing psuedocode for pygowave_server.core.tags.Tag.

--- pyofwave_server/core/tags.py
+++ pyofwave_server/core/tags.py
@@ -5,40 +5,51 @@ import delta, datasource
 
 class Tag(object):
    """Simplified document interface."""
-   def __init__(self, item):
+   def __init__(self, doc, item):
+      #provide default values if parameters not passed.
+      self._doc = doc
       self._item = item
       self._delta = Delta()
+
+      self._content = []
+      #retrieve all direct children of item from doc as tags
 
    @property
    def _name(self): return self._item.name
 
    #itterable properties
    def __len__(self):
-      pass
+      return len(self._content)
 
    def __getitem__(self, index):
-       pass
+       return self._content[i]
 
    def __setitem__(self, index, value):
-       pass
+       #if value is a string, change it into a Text object
+       self.content[i] = value
 
    #psuedo-properties
    def __getattr__(self, attr):
-        pass
+        return self._item.annotations[attr]
 
    def __setattr__(self, attr, value):
-        pass
+        #edit delta to set the attr
 
     def __delattr__(self, attr, value):
-        pass
+        #ensure delta is is a deltaBoundary
+        #edit delta to delete the attr
 
    #delta creation
-   @property
-   def _contentdelta(self):
-      pass
+   def _contentdelta(self, deltas):
+      deltas.append(self._delta)
+      for child in self._content: child._contentdelta(deltas)
 
    def sendDelta(self):
       """Sends a beta delta."""
+      from delta import betaDeltaObservable
+
+      #create delta
+      betaDeltaObservable.applyDelta(self._doc, delta)
 
 class Text(object):
    """Represents textual changes. """
