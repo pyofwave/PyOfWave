67497e3deffa6d141c8e55944ad694fb3abe9a69
alcinnz <alcinnz@eml.cc>
2011-04-17 14:51:33 -0700

    Edited pyofwave_client/pyofwave_client/pygowave_client.js via GitHub

--- pyofwave_client/pyofwave_client/pygowave_client.js
+++ pyofwave_client/pyofwave_client/pygowave_client.js
@@ -1,4 +1,7 @@
-/*Main file of PyGoWave client. Please acknowledge that I've chosen my own folder structure which I think will work well for PyGoWave over the preset.*/
+/*Main file of PyOfWave client. Please acknowledge that I've chosen my own folder structure which I think will work well for PyOfWave over the preset.*/
+
+/*Controller representing the full client area, excluding the toolbar.
+That is covered seperately.*/
 $.Controller('client', {
    
 });
