25f308c33701c745c9691d0b6ec1c3163bc7a905
alcinnz <alcinnz@eml.cc>
2011-04-17 15:03:01 -0700

    Edited pyofwave_client/pyofwave_client/api/operators.js via GitHub

--- pyofwave_client/pyofwave_client/api/operators.js
+++ pyofwave_client/pyofwave_client/api/operators.js
@@ -1,12 +1,17 @@
 /*Handlers to display element objects. TODO: rename to elements.js*/
+
+/*Mapping of Element types to handler functions.*/
 EL_TYPES = {
-   attachment : function(el) {
+   /*An attached file.*/
+   "attachment" : function(el) {
       return $('<iframe>').attr('src', el.url);
    },
-   gadget : function(el) {
+   /*A collaborative "gadget"*/
+   "gadget" : function(el) {
       return $('<iframe>').gadget(el.url, el.state);
    },
-   control : function(el) {
-      return $('<p>').append("control.html", el);
+   /*A form control.*/
+   "control" : function(el) {
+      return $('<p>').append("control.ejs", el);
    }
 }
