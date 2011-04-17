3f302cdd07917fa450d02a341884cf4af27bdfbd
alcinnz <alcinnz@eml.cc>
2011-04-17 14:59:44 -0700

    Edited pyofwave_client/pyofwave_client/api/kvo.js via GitHub

--- pyofwave_client/pyofwave_client/api/kvo.js
+++ pyofwave_client/pyofwave_client/api/kvo.js
@@ -1,14 +1,20 @@
 /*Small class which wraps dynamic objects to form loose coupling between model and view.*/
 
+/*Wraps a value and adds "observers"*/
 $.Class('KVO', {
+   /*Initializes KVO with an object.*/
    init : function(obj) {
       this._observers = new Array();
       this._binds = {}
       this.set(obj);
    }
+   /*Returns a property of this._obj*/
    get : function(key) {return this._obj[key];}
+   /*Adds a function as a observer and returns an "ID"*/
    observe : function(cb) {return this._observers.push(cb);}
+   /*Removes a function with an "ID".*/
    unobserve : function(id) {this._observers.remove(this._observers[id]);}
+   /*Sets this to refer to a different value and notifies observers.*/
    set : function(obj) {
       for (var id in this._binds) this.get(this._binds[id]).unobserve(id); //unbinds this from previous value
 
@@ -25,6 +31,7 @@ $.Class('KVO', {
 
       for (var o in observers) o(this);  //notify observers. 
    }
+   /*Observer for all children of the represented value.*/
    _notify : function(obj) {
       //called when a property of obj changes, via observer system. """
       var key;
