0251fa6cf55c0ea257427d53ee7d3386a77a2202
alcinnz <alcinnz@eml.cc>
2011-04-19 14:33:33 -0700

    Edited pyofwave_client/gadget/connect.js via GitHub

--- pyofwave_client/gadget/connect.js
+++ pyofwave_client/gadget/connect.js
@@ -2,19 +2,22 @@
 cons trustedSource; //To be provided by provider as their domain.
 
 //data
+/*Recieves messages from the gadget view.
 function displayMessage(message, source) {
    if (source != trustedSource) {
       alert("A external site (?) has attempted to update this gadget's data. The data has not changed.".replace("?", source));
       return;
    }
-   source = eval(source);
+   message = eval(message);
 
-   for (var key in source.public) publicState[key] = source.public[key];
-   for (key in source.private) privateState[key] = source.private[key];
+   for (var key in source.public) publicState[key] = message.public[key];
+   for (key in source.private) privateState[key] = message.private[key];
 }
+/*Send changes to the gadgetView as a comma seperated data of public,key,value*/
 publicState.observe(function(obj, key, val) {
    parent.postMessage("public,"+key+","+val, trustedSource);
 });
+/*Send changes to the gadgetView as a comma seperated data of private,key,value*/
 private.observe(function(obj, key, val) {
    parent.postMessage("public,"+key+","+val, trustedSource);
 });
