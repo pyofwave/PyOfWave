92ec653477d16f0e98576d124b55d9d801465fb8
alcinnz <alcinnz@eml.cc>
2011-04-22 09:06:13 +1200

    Implementing a client-side version of wQuery.

---
+++ pyofwave_client/pyofwave_client/api/elements.js
@@ -0,0 +1,17 @@
+/*Handlers to display element objects.*/
+
+/*Mapping of Element types to handler functions.*/
+EL_TYPES = {
+   /*An attached file.*/
+   "attachment" : function(el) {
+      return $('<iframe>').attr('src', el.url);
+   },
+   /*A collaborative "gadget"*/
+   "gadget" : function(el) {
+      return $('<iframe>').gadget(el.url, el.state);
+   },
+   /*A form control.*/
+   "control" : function(el) {
+      return $('<p>').append("control.ejs", el);
+   }
+}
---
+++ pyofwave_client/pyofwave_client/api/wquery-local.js
@@ -0,0 +1,32 @@
+/* Replace wquery.js with this file to unbind yourself from PyOfWave Server or any Wave Server.*/
+
+function sendOperations() {
+  for (var arg in arguments) {
+    res = operations[arg[0]](arg[1]);
+    arg[2](res);
+  }
+}
+
+//keep track of wavelets and blips for event handling use. 
+var wavelets = new Array();
+var blips = {}
+
+/*Wraps a wavelet with KVO and stores it for later access.
+function wavelet(obj) {
+   obj = new KVO(obj);
+   wavelets.push(obj);
+   return obj;
+}
+/*Same as above but for blips.*/
+function blip(obj) {
+   obj = new KVO(obj);
+   blips[obj.get("blipId")] = obj;
+   return obj;
+}
+
+//Domains for protocol local protocol routines
+var eventHandlers = {}
+
+var operations = {
+  
+}
