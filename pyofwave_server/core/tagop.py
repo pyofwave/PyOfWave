6c4adb1ecea0f5c667bf096c4509ec5c858928a3
alcinnz <alcinnz@eml.cc>
2011-04-21 14:19:07 +1200

    Implementing tag interface on deltas.

---
+++ pyofwave_server/core/tagop.py
@@ -0,0 +1,23 @@
+"""
+Alternate delta routines for easier visualising.
+"""
+
+def retain(old, new, itemCount):
+
+def updateAtrributes(old, new, oldAttributes, newAttributes):
+
+def replaceAttributes(old, new, attributesUpdate):
+
+def charactors(old, new, charactors):    
+
+def elementStart(old, new, typeI, attrs):
+
+def elementEnd(old, new):
+
+def deleteCharactors(old, new, charactors):
+
+def deleteElementStart(old, new, typeI, attrs):
+
+def deleteElementEnd(old, new):
+
+def annotationsBoundary(old, new, ends, changes):
