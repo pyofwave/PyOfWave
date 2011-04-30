7a079ab7d2e65c9c6abbdd1a2bf5a24f50771f58
alcinnz <alcinnz@eml.cc>
2011-04-30 15:10:20 -0700

    Edited pyofwave_client/pyofwave_client/wave.js via GitHub

--- pyofwave_client/pyofwave_client/wave.js
+++ pyofwave_client/pyofwave_client/wave.js
@@ -8,13 +8,20 @@ if (uri.querykey.alias) loginAlias(uri.querykey.alias);
 
 if (uri.host == "__new__") {
    // Send operations to create a new wave with URI options. 
+   var wavelet = {} // TODO: fill out. 
+   sendOperations(["robot.createWavelet", {waveletData : wavelet, message : "URL create"}, handleResponse]);
 }
 else {
    sendOperations(
-      ["robot.fetchWavelet", {waveId : uri.host, waveletId : uri.host, message : "read"
+      ["robot.fetchWavelet", {waveId : uri.host, waveletId : uri.host, message : "URL read"}, handleResponse]);
 }
 });
 
 function loginAlias(alias) {
    
+}
+
+function handleResponse(data) {
+   //index data
+   $('#wave-root').waveletView({wavelet: wavelet});
 }
