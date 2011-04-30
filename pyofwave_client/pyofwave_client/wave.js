09dc55aaee75978986a168bf2248b6d500b3c672
alcinnz <alcinnz@eml.cc>
2011-04-30 14:48:11 -0700

    Edited pyofwave_client/pyofwave_client/wave.js via GitHub

--- pyofwave_client/pyofwave_client/wave.js
+++ pyofwave_client/pyofwave_client/wave.js
@@ -3,9 +3,17 @@ steal('../api/waveUI.js', '../api/uri.js').then(function() {
 uri = parseUri(location.hash);
 
 //manage options
+if (uri.querykey.css) steal().css(uri.querykey.css);
+if (uri.querykey.alias) loginAlias(uri.querykey.alias);
 
-//setup interface
-
-//display specified wave
-
+if (uri.host == "__new__") {
+   // Send operations to create a new wave with URI options. 
+}
+else {
+   // Send operations to retrieve uri.host
+}
 });
+
+function loginAlias(alias) {
+   
+}
