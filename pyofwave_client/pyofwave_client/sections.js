d9d07476035a4fe2e5e94a10b592d40bff33d0cc
alcinnz <alcinnz@eml.cc>
2011-05-05 02:14:40 -0700

    Edited pyofwave_client/pyofwave_client/sections.js via GitHub

--- pyofwave_client/pyofwave_client/sections.js
+++ pyofwave_client/pyofwave_client/sections.js
@@ -1,8 +1,36 @@
-/*The controller for the toolbar options.*/
+/* Provides functions managing each section of the UI. */
 
-/*Widget representing toolbar options.*/
-steal('../api/jquery/jquerymx-1.0.min.js').then(function() {
-$.Controller('toolbar', {
-   
-});
-});
+// TODO: fill in utilities. 
+
+function inboxes() {
+   function inbox(obj) {
+      //generate list items. 
+   }
+   // Load the root inbox. 
+
+   return $('<ul>').append(inbox(root)).tree();
+}
+inboxes.toolbar = [
+   $('<input type="search" name="search" value="in:inbox" />').submit(search);
+]
+
+function waves() {
+   return $('<table>').table({rowLoader : waveLoader, 
+      headers : ["unread", "sample", "last changed"],
+      rowClick : showWave});
+}
+inbox.toolbar = [
+   // TODO: full in options.
+]
+
+function wave() {
+   return $('<div><div id="toolbar"></div>').append(
+      $('<div>').BlipView({blipId : selwave.rootBlipId}));
+}
+wave.toolbar = [
+   $('<div>').participantsListView({participants : selwave.participants}),
+   $('<img>').attr('href', "playback.png").click(playback);
+   $('<img>').attr('href', "rss.png").click(follow("RSS")),
+   $('<img>').attr('href', "popout.png").click(follow('HTML'))
+] 
+]
