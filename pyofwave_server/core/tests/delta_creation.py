ae949e759c5d03a91ece480ae3783130e23880b2
Adrian Cochrane <alcinnz@eml.cc>
2011-05-26 18:21:04 +1200

    Ensuring deltas can be created.

--- pyofwave_server/core/tests/delta_creation.py
+++ pyofwave_server/core/tests/delta_creation.py
@@ -5,3 +5,5 @@ Testing script to ensure I can initialize Delta and Operation objects.
 from .. import delta
 
 delta.Operation("test", "spam", "eggs")
+
+delta.Delta(delta.Operation("test", "spam", "eggs", 42))
