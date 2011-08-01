bee50c66318ff8e8a7cad297cb3bd4f72821ea4f
alcinnz <alcinnz@eml.cc>
2011-08-01 18:39:00 +1200

    Changing import to use Ajax.

--- pyofwave_client/api/import.js
+++ pyofwave_client/api/import.js
@@ -1,7 +1,12 @@
-/* Simple JavaScript which imports javascripts during development, imports are replaced by the script by compress.py for production. 
- * Should not be used in production, internet delay may (probably will) cause bugs. */
+/* Simple JavaScript which imports javascripts during development, imports are replaced by the script by compress.py for production. */
 function import() {
   for (var i; i < arguments.length; i++) {var m = arguments[i];
-    $('head').append($('<script>').attr('type', 'text/javascript').attr('src', m));
+    var request = new XMLHTTPRequest();
+    request.open("get", m, false);
+    request.send();
+    eval(request.responseText);
+
+    import.loaded.push(m);
   }
 }
+import.loaded = new Array();
