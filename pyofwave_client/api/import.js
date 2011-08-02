598ce085ea8b2b93b4e52435115d9f9f16248e2e
Adrian Cochrane <alcinnz@eml.cc>
2011-08-02 18:11:13 +1200

    Reverting to import to <script> approach for environment.

--- pyofwave_client/api/import.js
+++ pyofwave_client/api/import.js
@@ -1,12 +1,10 @@
-/* Simple JavaScript which imports javascripts during development, imports are replaced by the script by compress.py for production. */
+/* Simple JavaScript which imports javascripts during development, imports are replaced by the script by compress.py for production. 
+ * Should not be used in production, internet delay may (probably will) cause bugs. */
 function import() {
   for (var i; i < arguments.length; i++) {var m = arguments[i];
-    var request = new XMLHTTPRequest();
-    request.open("get", m, false);
-    request.send();
-    new Function(request.responseText)()
-
-    import.loaded.push(m);
+    document.write("<script type='text/javascript' src='"+ m +"'></scr"+"ipt>");
   }
+  // waste some time in waiting
+  for (var i; i < 500; i++) {}
+  // alert("Waiting for scripts to load. . ."); // uncomment if import problems are experienced
 }
-import.loaded = new Array();
