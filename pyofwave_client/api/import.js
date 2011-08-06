e7d0eea2151a3355962cb1d713a57efc231d2d5b
Adrian Cochrane <alcinnz@eml.cc>
2011-08-06 15:34:00 +1200

    Adding testing.

--- pyofwave_client/api/import.js
+++ pyofwave_client/api/import.js
@@ -1,10 +1,34 @@
 /* Simple JavaScript which imports javascripts during development, imports are replaced by the script by compress.py for production. 
- * Should not be used in production, internet delay may (probably will) cause bugs. */
-function import() {
-  for (var i; i < arguments.length; i++) {var m = arguments[i];
-    document.write("<script type='text/javascript' src='"+ m +"'></scr"+"ipt>");
+ * Should not be used in production.
+/* This script will be the only place we pamper to IE, and doesn't use jQuery since we'll import that. */
+(function() {
+  var request = null;
+  var loaded = [];
+
+  // create the request object.
+  try {
+    request = new XMLHttpRequest();
+  } catch (trymicrosoft) {
+    try {
+      request = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (othermicrosoft) {
+      request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
   }
-  // waste some time in waiting
-  for (var i; i < 500; i++) {}
-  // alert("Waiting for scripts to load. . ."); // uncomment if import problems are experienced
-}
+
+  // import script
+  window.import = function() {
+    for (var script in arguments) {
+      if (loaded.indexOf(script) <= 0) {
+        request.open("GET", script, false);
+        request.send();
+
+        (window.execScript || function(data) {
+          window["eval"].call(window, data);
+        })(request.responseText);
+
+        loaded.push(script);
+      }
+    }
+  }
+})();
---
+++ pyofwave_client/testing/spec/interface.js
@@ -0,0 +1,1 @@
+var imported = true;
