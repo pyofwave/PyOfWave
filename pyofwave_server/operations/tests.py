f87434d8b01f5750d1ac3e21f174d5cf49b14d22
Adrian Cochrane <alcinnz@eml.cc>
2011-08-11 20:57:32 +1200

    Adding more test cases to PyOfWave Client Protocol.

--- pyofwave_server/operations/tests.py
+++ pyofwave_server/operations/tests.py
@@ -1,2 +1,16 @@
 def operation(ip, foo):
     """A simple Operation to test against."""
+    
+def result(ip, foo, bar):
+    """A simple Operation to test return values against."""
+    return {'Star Trek' : {"Spock": "spock@federation.gov",},
+            'Firefly' : {"Malcom" : "malcom@serenity.ship",
+                         "Shepherd" : "shepherd@serenity.ship"},
+            'Doctor Who' : {"The Master" : "master@timelords.gal",},
+            'Star Wars' : {"Yoda" : "yoda.jedi.org",}
+            }
+
+def error(ip):
+    """A simple Operation to test errors against."""
+    from .. import operations
+    raise operations.OperationError(500, cause="I can't let you do that.",  reason="DON'T SHUT ME DOWN!")
