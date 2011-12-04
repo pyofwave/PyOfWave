18fa56504f4d5f5c71a9103dc903e75fd1d5ab58
Adrian Cochrane <alcinnz@eml.cc>
2011-12-05 11:18:19 +1300

    tidies.

--- pyofwave_server/setup.py
+++ pyofwave_server/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='PyOfWave',
       version='0.1',
       description='Open Source Wave server in Python',
-      author=u'Alcinnz',
+      author=u'alcinnz (Adrian Cochrane)',
       author_email='alcinnz@eml.cc',
       url='https://github.com/pyofwave/PyOfWave',
       license='Mozilla Public License Version 2.0, Release Candidate 1',
