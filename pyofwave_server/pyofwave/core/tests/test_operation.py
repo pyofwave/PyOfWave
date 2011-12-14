02e5bed50986e9cfc7c4240d67f04b7ff5ccb150
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-12-14 17:04:00 +0100

    Update test

--- pyofwave_server/pyofwave/core/tests/test_operation.py
+++ pyofwave_server/pyofwave/core/tests/test_operation.py
@@ -11,31 +11,25 @@ class TestOperations(unittest.TestCase):
 
         @NS
         def op(event, arg, tag, text, action):
-            self.assertEqual(event, None)
+            self.assertEqual(event, "AnEventToTrigger")
             self.assertEqual(arg, "Hello")
             self.assertEqual(tag.tag, "{pyofwave.info/test}tag")
             self.assertEqual(text, "World")
             self.assertEqual(action, "SayHello")
             return NS.E.response("success", status = "400")
-    
-        operation.performOperation(None,
-                                   E.op("Hello",
-                                        E.tag(),
-                                        "World",
-                                        action="SayHello"))
 
     def testPerformOperation(self):
-        res = operation.performOperation(None,
-                                   E.op("Hello",
-                                        E.tag(),
-                                        "World",
-                                        action="SayHello"))
+        res = operation.performOperation("AnEventToTrigger",
+                                         E.op("Hello",
+                                              E.tag(),
+                                              "World",
+                                              action="SayHello"))
         
         self.assertEqual(res.text, "success")
         self.assertEqual(res.get("status"), "400")
 #
-class TestEvents(unittest.TestCase):
-    def testOperationEvents(self):
+class TestEventRegisty(unittest.TestCase):
+    def testEventRegisty(self):
         #Define a test context
         res = {"value": "FOO"}
         #Define a function used as event callback that will modify the context
@@ -44,10 +38,10 @@ class TestEvents(unittest.TestCase):
         
         url = "wave://test@pyofwave.info/test"
         user="test@pyofwave.info"
-        evts = operation.Events(user=user, 
-                                callback=misc)
+        event_registry = operation.EventRegisty(user=user, 
+                                                callback=misc)
         
-        self.assertEqual(evts.user, user)
+        self.assertEqual(event_registry.user, user)
 
         # Create an operation for it's event.
         NS = opdev.OperationNS("pyofwave.info/test")
@@ -55,29 +49,29 @@ class TestEvents(unittest.TestCase):
         def op(event, *args, **kwargs): pass
 
         # Test that event isn't triggered before register
-        operation.performOperation(evts, E.op(href=url))
+        operation.performOperation("AnEventToTrigger", E.op(href=url))
         self.assertEqual(res["value"],"FOO")
 
         # Trigger event
-        evts.register(url, "{pyofwave.info/test}op")
-        operation.performOperation(evts, E.op(href=url))
+        event_registry.register(url, "{pyofwave.info/test}op")
+        operation.performOperation("AnEventToTrigger", E.op(href=url))
         self.assertEqual(res["value"], "BAR")
 
         # test different URL
         res["value"] = "FOOBAR"
-        operation.performOperation(evts, E.op(href="pyofwave.info/Firefly"))
+        operation.performOperation("AnEventToTrigger", E.op(href="pyofwave.info/Firefly"))
         self.assertEqual(res["value"], "FOOBAR")
 
         # Unregister one event
         res["value"] = "BARFOO"
-        evts.unregister(url, "{pyofwave.info/test}op")
-        operation.performOperation(evts, E.op(href=url))
+        event_registry.unregister(url, "{pyofwave.info/test}op")
+        operation.performOperation("AnEventToTrigger", E.op(href=url))
         self.assertEqual(res["value"], "BARFOO")
         
         # Unregister all events
-        evts.register(url, "{pyofwave.info/test}op1")
-        evts.register(url, "{pyofwave.info/test}op2")
-        evts.unregister(url, "*")
+        event_registry.register(url, "{pyofwave.info/test}op1")
+        event_registry.register(url, "{pyofwave.info/test}op2")
+        event_registry.unregister(url, "*")
         
 
 if __name__ == '__main__':
