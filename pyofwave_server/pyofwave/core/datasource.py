cdd9715de9225ea04a8166e78444c0089d6f697b
Guillaume Libersat <glibersat@sigill.org>
2011-12-16 21:48:06 +0800

    use NotImplementedError and cleanups for DataSource Interface

--- pyofwave_server/pyofwave/core/datasource.py
+++ pyofwave_server/pyofwave/core/datasource.py
@@ -1,29 +1,47 @@
 """
 Contains standard interfaces for dataSources. 
 """
-import delta
 from zope import interface
 
 class DataSource(interface.Interface):
-    """Standard interface for a dataSource. """
+    """
+    Standard interface for a dataSource. 
+    """
     def newDocument(doc):
-        """generate a new document."""
+        """
+        generate a new document.
+        """
+        raise NotImplementedError
 
     def getDocument(doc):
-        """returns the specified document."""
+        """
+        returns the specified document.
+        """
+        raise NotImplementedError
 
     def getDocumentVersion(doc, start, end, limit):
-        """Returns the delta for the specified versions. """
+        """
+        Returns the delta for the specified versions. 
+        """
+        raise NotImplementedError
 
     def searchDocuments(user, query):
-        """Returns an iterable of all documents that match the query.
-        This query  should be in the form found in the client protocol standard."""
+        """
+        Returns an iterable of all documents that match the query.
+        This query should be in the form found in the client protocol
+        standard.
+        """
+        raise NotImplementedError
 
     def setTags(doc, user, **tags):
-        """Apply the tags to the document/user combination to be searched."""
+        """
+        Apply the tags to the document/user combination to be searched.
+        """
+        raise NotImplementedError
 
     def applyDelta(doc, delta):
-        """Apply delta to doc and save."""
-        
-    successor = interface.Attribute('DataSource')
+        """
+        Apply delta to doc and save.
+        """
+        raise NotImplementedError
 
