893922825bf54908764104e66c9ba0009ee004ba
Guillaume Libersat <glibersat@sigill.org>
2011-12-22 19:30:00 +0800

    better Document implementation

--- pyofwave_server/pyofwave/core/document.py
+++ pyofwave_server/pyofwave/core/document.py
@@ -1,15 +1,47 @@
+import pyofwave.storage
+
 class Document(object):
     """
     A document is piece of XML linked to an URI, that can be stored
     into a datastore and on which operations can be applied to
     transform it.
     """
-    def __init__(self, uri, content=""):
+    def __init__(self, uri, content="", aDataStore=None):
         self.uri = uri
         self.content = content
+        self.datastore = aDataStore
 
+    @classmethod
+    def load(klass, uri, aDataStore=None):
+        """
+        Loads a document from a given datastore, or from the
+        configured default one.
+        """
+        if not aDataStore:
+            aDataStore = pyofwave.storage.datastore
+
+        return aDataStore.getDocument(uri)
+        
+
+    def save(self, aDataStore=None):
+        """
+        Persist a Document into the given DataStore, or the one where
+        the document comes from.
+        """
+        if not aDataStore:
+            if self.datastore:
+                aDataStore = self.datastore
+            else:
+                # XXX: Use a real exception
+                raise "This document is not linked to a datastore or none was specified"
+            
+        return aDataStore.saveDocument(self)
+                
     @property
     def length(self):
+        """
+        The length of the xml content
+        """
         return len(self.content)
 
     def __str__(self):
