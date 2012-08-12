e6d1f82e652185521d8f5554d6f08c542accb259
Adrian Cochrane <alcinnz@eml.cc>
2012-08-12 21:37:49 +1200

    Got popovers to work as jQuery plugin and Require.js module, started JS menu interface.

--- pyofwave_client/proto/modal.js
+++ pyofwave_client/proto/modal.js
@@ -1,11 +1,11 @@
-define(['../src/api/jquery-1.5.1.js', '../src/api/jqueryUI/jquery-ui-1.8.14.custom.js', '../src/api/farbtastic.js'], function($) {
+define(['../src/api/jquery-1.5.1.js', '../src/api/jqueryUI/jquery-ui-1.8.14.custom.js', '../src/api/farbtastic.js'], function() {
 	var $overlay = $('<div>').css({
 				position: 'absolute', top: 0, left: 0, 
 				width: window.innerWidth, height: window.innerHeight, 
 				background: 'black', opacity: 0.1})
 
-		$.fn.popover = function popover($el) {
-			$el.hide()
+		$.fn.popover = function popover() {
+			var $el = $(this).hide()
 			return function(evtOrEl) {
 				$el.fadeIn().click(function(evt) {evt.stopPropagation()})
 					.position({of: evtOrEl, offset: "0 10", collision: "fit",
@@ -20,4 +20,13 @@ define(['../src/api/jquery-1.5.1.js', '../src/api/jqueryUI/jquery-ui-1.8.14.cust
 		$(function() {
 			$overlay.prependTo('body').hide()
 		})
-}
+		return {
+			menu: function(options) {
+				$el = $('<ul>')
+				$.each(options, function(key, value) {
+					// TODO: generate 
+				})
+				return $el.popover()
+			}
+		}
+})
